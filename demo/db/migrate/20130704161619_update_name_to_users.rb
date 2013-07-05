aec59d7206d8f45c693003a10c5eeda18ac75453
taher-rampurawala <taherhi8@gmail.com>
2013-07-05 22:22:03 +0530

    remove demo

--- demo/db/migrate/20130704161619_update_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class UpdateNameToUsers < ActiveRecord::Migration
-  def change
-	rename_column :users, :name, :firstname
-  end
-end
--- demo/db/migrate/20130704175841_create_posts.rb
+++
@@ -1,9 +0,0 @@
-class CreatePosts < ActiveRecord::Migration
-  def change
-	create_table :posts do |t|
-	t.belongs_to :users
-	t.string :post_name
-	t.timestamps
-    end
-  end
-end
